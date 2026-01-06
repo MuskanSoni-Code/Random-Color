1ceffb27819e5c792402ad1f92a221b074f3ba70
Muskan Soni <sonimuskan9451@gmail.com>
2026-01-06 21:14:05 +0530

    Add script to change background color randomly

---
+++ Style.js
@@ -0,0 +1,9 @@
+    <script>
+      bgcolor = () => {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+      };
+    </script>
+  
